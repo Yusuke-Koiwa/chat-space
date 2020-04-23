6556c90e28c270220898259e9436c4a8e5e6e24c
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-04-23 17:31:00 +0900

    create message-function

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -27,17 +27,18 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [800, 800]
+    # process resize_to_fit: [800, 800]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  process resize_to_fit: [200, 200]
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.

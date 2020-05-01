4dd1ac6ff64b38913f1b3b18640c7c9bc3b792a0
Yusuke-Koiwa <smallrock0109@gmail.com>
2020-05-01 09:12:08 +0900

    Improve incremental search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,18 @@
 $(function(){
+
   function addUser(user){
+    if ( $("#chat-group-users.js-ass-user").find('.chat-group-user').hasClass(`add-user-${user.id}`) ) {
+      var html = 
+        `<div class="chat-group-user clearfix">
+          <p class="chat-group-user__name">ユーザーが見つかりません</p>
+        </div>`
+    } else {
     var html = 
-      `<div class="chat-group-user clearfix">
+      `<div class="chat-group-user add-user-${user.id} clearfix">
         <p class="chat-group-user__name">${user.name}</p>
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>`
+    }
     $('#user-search-result').append(html);
   }
 
@@ -18,10 +26,10 @@ $(function(){
 
   function addUserToMember(id, name) {
     var html =
-      `<div class='chat-group-user'>
+      `<div class='chat-group-user add-user-${id}'>
         <input name='group[user_ids][]' type='hidden' value='${id}'>
         <p class='chat-group-user__name'>${name}</p>
-        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+        <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn data-user-id="${id}" data-user-name="${name}'>削除</div>
       </div>`
     $('#chat-group-users.js-ass-user').append(html);
   }
